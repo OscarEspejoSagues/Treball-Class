11f4d3168b4e21abd898f017d1a2e9ad2f529a40
oscar espejo <xxgreysoulxx@gmail.com>
2017-09-26 10:30:47 +0200

    Template & Function Overload

--- Treball Class/Source.cpp
+++ Treball Class/Source.cpp
@@ -1,6 +1,76 @@
 #include <iostream>
 #include <forward_list>
 
+/*
+Sobrecarga de funciones
+
+	int test(){}
+	int test(int a){}
+	float test(doble a){}
+
+Ejemplo:
+===================================
+	int sum (int a, int b){
+		return a+b;
+	}
+
+	doble sum (doble a, doble b){
+		return a+b;
+	}
+
+	int main(){
+		cout<<sum(10,20);
+		cout<<sum(1.0, 1.5);
+		return 0;
+	}
+====================================
+
+
+Firma --> void lmao (int a, int b) siempre diferente si hay varias funciones que se llaman igual
+		  void lmao (int a, float b) funciones con el mismo nombre pero diferente firma
+
+Overloaded function--> familia de funciones con el mismo nombre que tienen firmas diferentes
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+Function Template
+
+    - Familia de funciones con una unica funcion, pero que acepta argumentos diferentes PLANTILLA
+	- Podre usar esto con cualquier tipo? Mientras la funcion permita a esa variable la funcion definida, como por ejemplo el =, o el <, si podras
+
+Ejemplo:
+===================================
+
+	? absolute (? a){
+		if(a<0){
+			a = -a;
+		}
+		return a;
+	};
+
+	----------------------
+
+	template<typename T>
+
+	T absolute (T a){
+		if(a<0){
+			a = -a;
+		}
+		return a;
+	};
+	---------------------
+	EN EL MAIN LO LLAMARAS ASI:
+	absolute<int>(4);
+	absolute<float>(4.0); haria una comversion automatica para que pueda comprarlo 0.0 
+
+===================================
+
+
+*/
+
+
+template <typename T>;
 
 
 void main() {
